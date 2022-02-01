0a16dee515111643119feb3f9946785b713d917b
BasharAbuHannoud <abuhannoud.bashar@gmail.com>
2022-02-01 10:56:35 +0200

    Add strter code

---
+++ backend/database/db.js
@@ -0,0 +1,16 @@
+const mysql = require("mysql2");
+
+const connection = mysql.createConnection({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.DB_NAME,
+});
+
+connection.connect((err) => {
+  if (err) {
+    console.error("error connecting: " + err.stack);
+    return;
+  }
+  console.log("connected as id: " + connection.threadId);
+});
