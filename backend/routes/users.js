edd6fdf29ea338f3efc7d1ded80544f5f9f3d12a
kholoud ibrahim <ahlam.aljawahreh@gmail.com>
2022-02-01 19:25:47 +0200

    Update Profile Image

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -1,9 +1,10 @@
 const express = require("express");
 
-const { createNewUser } = require("../controllers/users");
+const { createNewUser ,updateProfilePhoto} = require("../controllers/users");
 
 const usersRouter = express.Router();
 
 usersRouter.post("/", createNewUser);
+usersRouter.put("/image/:id",updateProfilePhoto)
 
 module.exports = usersRouter;
