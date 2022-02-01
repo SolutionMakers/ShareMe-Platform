1828f809cec3a422aa07878a96ce427b6ed675a7
kholoud ibrahim <ahlam.aljawahreh@gmail.com>
2022-02-01 12:52:51 +0200

    create users table

--- backend/database/schema.sql
+++ backend/database/schema.sql
@@ -1,1 +1,16 @@
+DROP DATABASE MERAKI_Academy_Project_5;
 CREATE DATABASE MERAKI_Academy_Project_5;
+
+CREATE TABLE users (
+    id INT AUTO_INCREMENT NOT NULL,
+    userName VARCHAR(255) UNIQUE NOT NULL,
+    email VARCHAR(255) UNIQUE NOT NULL,
+    password VARCHAR(255) NOT NULL,
+    dob DATETIME ,
+    country VARCHAR(255),
+    role_id INT,
+    profileimage VARCHAR(255),
+    gender VARCHAR(255),
+    FOREIGN KEY (role_id) REFERENCES roles(id),
+    is_deleted TINYINT DEFAULT 0,
+    PRIMARY KEY (id));
