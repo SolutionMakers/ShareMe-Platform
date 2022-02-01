7c91f5385be26c09a8cb3b72266aef3150f2b61c
BasharAbuHannoud <abuhannoud.bashar@gmail.com>
2022-02-01 15:46:35 +0200

    Update database

--- backend/database/schema.sql
+++ backend/database/schema.sql
@@ -1,17 +1,26 @@
 DROP DATABASE MERAKI_Academy_Project_5;
+
 CREATE DATABASE MERAKI_Academy_Project_5;
+
 USE MERAKI_Academy_Project_5;
+
+CREATE TABLE roles (
+    id INT AUTO_INCREMENT NOT NULL,
+    role VARCHAR(255) NOT NULL UNIQUE,
+    is_deleted TINYINT DEFAULT 0,
+    PRIMARY KEY (id)
+);
 
 CREATE TABLE users (
     id INT AUTO_INCREMENT NOT NULL,
     userName VARCHAR(255) UNIQUE NOT NULL,
     email VARCHAR(255) UNIQUE NOT NULL,
     password VARCHAR(255) NOT NULL,
-    dob DATETIME ,
+    dob DATETIME,
     country VARCHAR(255),
-    role_id INT,
     profileimage VARCHAR(255),
     gender VARCHAR(255),
+    role_id INT,
     FOREIGN KEY (role_id) REFERENCES roles(id),
     is_deleted TINYINT DEFAULT 0,
     PRIMARY KEY (id)
@@ -21,12 +30,8 @@ CREATE TABLE posts (
     id INT AUTO_INCREMENT NOT NULL,
     description VARCHAR(255),
     user_id INT,
-    comment_id INT,
     FOREIGN KEY (user_id) REFERENCES users(id),
-    FOREIGN KEY (comment_id) REFERENCES comments(id),
-    media VARCHAR(255)
-    like_id INT,
-    FOREIGN KEY (like_id) REFERENCES likes(id),
+    media VARCHAR(255),
     is_deleted TINYINT DEFAULT 0,
     PRIMARY KEY (id)
 );
@@ -34,29 +39,20 @@ CREATE TABLE posts (
 CREATE TABLE comments(
     id INT AUTO_INCREMENT NOT NULL,
     comment VARCHAR(255),
-    post_id INT,
-    FOREIGN KEY (post_id) REFERENCES posts(id),
     commenter_id INT,
     FOREIGN KEY (commenter_id) REFERENCES users(id),
-    is_deleted TINYINT DEFAULT 0,
-    PRIMARY KEY (id)
-);
-
-
-CREATE TABLE roles (
-    id INT AUTO_INCREMENT NOT NULL,
-    role VARCHAR(255) NOT NULL UNIQUE,
+    post_id INT,
+    FOREIGN KEY (post_id) REFERENCES posts(id),
     is_deleted TINYINT DEFAULT 0,
     PRIMARY KEY (id)
 );
 
 CREATE TABLE likes(
     id INT AUTO_INCREMENT NOT NULL,
-    users_id INT,
-    FOREIGN KEY (users_id) REFERENCES users(id),
     post_id INT,
     FOREIGN KEY (post_id) REFERENCES posts(id),
+    user_id INT,
+    FOREIGN KEY (user_id) REFERENCES users(id),
     is_deleted TINYINT DEFAULT 0,
     PRIMARY KEY (id)
-)
-
+);
