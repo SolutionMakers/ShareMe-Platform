bae77b99cc7921ee20cb8b24635c025f6d066144
kholoud ibrahim <ahlam.aljawahreh@gmail.com>
2022-02-01 17:56:59 +0200

    edit login feature

--- backend/routes/login.js
+++ backend/routes/login.js
@@ -4,6 +4,6 @@ const { login } = require("../controllers/login");
 
 const loginRouter = express.Router();
 
-loginRouter.post("/login", login);
+loginRouter.post("/", login);
 
 module.exports = loginRouter;
