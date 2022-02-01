4437acbb64b8f4a3d42bc75ac80ea8d241f71584
AbedalelahHyari <abedalelah.hyari@gmail.com>
2022-02-01 17:53:10 +0200

    Create the users route

---
+++ backend/routes/users.js
@@ -0,0 +1,9 @@
+const express = require("express");
+
+const { createNewUser } = require("../controllers/users");
+
+const usersRouter = express.Router();
+
+usersRouter.post("/", createNewUser);
+
+module.exports = usersRouter;
--- backend/server.js
+++ backend/server.js
@@ -3,12 +3,14 @@ const cors = require("cors");
 require("dotenv").config();
 const app = express();
 const db = require("./database/db");
-const loginRouter = require ('./routes/login')
+const loginRouter = require("./routes/login");
+const usersRouter = require("./routes/users");
 app.use(cors());
 
 app.use(express.json());
 
-app.use('/login',loginRouter)
+app.use("/login", loginRouter);
+app.use("/users", usersRouter);
 
 const PORT = 5000;
 
