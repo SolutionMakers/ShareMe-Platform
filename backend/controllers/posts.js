fcc0f6de0862b7ba5d941b4c1d580b1dc892e8bb
AbedalelahHyari <abedalelah.hyari@gmail.com>
2022-02-01 19:59:23 +0200

    Create Get All Posts function

--- backend/controllers/posts.js
+++ backend/controllers/posts.js
@@ -1,20 +1,20 @@
 const connection = require("../database/db");
 
 const createNewPost = (req, res) => {
-  const { description,media } = req.body;
+  const { description, media } = req.body;
   const user_id = req.token.userId;
   const query = `INSERT INTO posts (description,user_id,media) VALUES (?,?,?);`;
-  const data = [description, user_id,media];
+  const data = [description, user_id, media];
 
   connection.query(query, data, (err, results) => {
     if (err) {
-    return  res.status(500).json({
+      return res.status(500).json({
         success: false,
         massage: "Server error",
         err: err,
       });
     }
-   
+
     res.status(200).json({
       success: true,
       massage: "Post created",
@@ -22,7 +22,28 @@ const createNewPost = (req, res) => {
     });
   });
 };
+/****************************************/
+const getAllPosts = (req, res) => {
+  const query = `SELECT * FROM posts WHERE is_deleted=0;`;
+
+  connection.query(query, (err, result) => {
+    if (err) {
+      res.status(500).json({
+        success: false,
+        massage: "server error",
+        err: err,
+      });
+    }
+    res.status(200).json({
+      success: true,
+      massage: "All the posts",
+      results: result,
+    });
+  });
+};
+/********************************** */
 
 module.exports = {
-    createNewPost,
+  createNewPost,
+  getAllPosts,
 };
