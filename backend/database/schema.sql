600acc17a60826d58e4720750d61434241e24962
BasharAbuHannoud <abuhannoud.bashar@gmail.com>
2022-02-01 12:54:34 +0200

    Add comments Table

--- backend/database/schema.sql
+++ backend/database/schema.sql
@@ -1,1 +1,12 @@
 CREATE DATABASE MERAKI_Academy_Project_5;
+
+CREATE TABLE comments(
+    id INT AUTO_INCREMENT NOT NULL,
+    comment VARCHAR(255),
+    post_id INT,
+    FOREIGN KEY (post_id) REFERENCES posts(id),
+    commenter_id INT,
+    FOREIGN KEY (commenter_id) REFERENCES users(id),
+    is_deleted TINYINT DEFAULT 0,
+    PRIMARY KEY (id)
+);
