0a38f2ef4457306aedc352a197ae52790bc680f9
AbedalelahHyari <abedalelah.hyari@gmail.com>
2022-02-01 22:03:26 +0200

    Create the funvtion called updatePostById

--- backend/routes/post.js
+++ backend/routes/post.js
@@ -1,18 +1,20 @@
 const express = require("express");
 
-
-
-const { createNewPost, getAllPosts,getPostsbyUserId,getPostById } = require("../controllers/posts");
+const {
+  createNewPost,
+  getAllPosts,
+  getPostsbyUserId,
+  getPostById,
+  updatePostById,
+} = require("../controllers/posts");
 const { authentication } = require("../middleware/authentication");
-
-
 
 const postsRouter = express.Router();
 
 postsRouter.post("/", authentication, createNewPost);
-postsRouter.get("/:user_id",getPostsbyUserId)
+postsRouter.get("/:user_id", getPostsbyUserId);
 postsRouter.get("/", getAllPosts);
 postsRouter.get("/:id/post", getPostById);
+postsRouter.put("/:id/post", updatePostById);
 
 module.exports = postsRouter;
-
