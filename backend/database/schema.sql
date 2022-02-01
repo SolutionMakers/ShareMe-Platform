18b92ebeda03c4ebfc4a07c72cd7fc38f1c54bea
AbedalelahHyari <abedalelah.hyari@gmail.com>
2022-02-01 12:53:14 +0200

    Create the posts table

--- backend/database/schema.sql
+++ backend/database/schema.sql
@@ -1,1 +1,15 @@
 CREATE DATABASE MERAKI_Academy_Project_5;
+
+CREATE TABLE posts (
+    id INT AUTO_INCREMENT NOT NULL,
+    description VARCHAR(255),
+    user_id INT,
+    comment_id INT,
+    FOREIGN KEY (user_id) REFERENCES users(id),
+    FOREIGN KEY (comment_id) REFERENCES comments(id),
+    media VARCHAR(255),
+    like_id INT,
+    FOREIGN KEY (like_id) REFERENCES likes(id),
+    is_deleted TINYINT DEFAULT 0,
+    PRIMARY KEY (id)
+);
