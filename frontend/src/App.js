0b7da92ecf6e5619cc1f597a193ce4a70c296610
kholoud ibrahim <ahlam.aljawahreh@gmail.com>
2022-02-02 22:12:11 +0200

    Single Post Component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,14 @@
 import "./App.css";
 import { Route, Router, Routes } from "react-router-dom";
 import Login from "./components/Login";
+import SinglePostPage from "./components/SinglePostPage";
 const App = () => {
   return (
     <div className="App">
   
 <Routes>
 <Route path="/" element={<Login/>}/>
+<Route path="/post/:id" element ={<SinglePostPage/>}/>
 
 
 </Routes>
