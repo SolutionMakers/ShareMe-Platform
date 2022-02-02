1419dab43c784ad995fe9b430d8d2eedff250a25
kholoud ibrahim <ahlam.aljawahreh@gmail.com>
2022-02-02 14:43:00 +0200

    login functionality

--- frontend/src/components/Login.js
+++ frontend/src/components/Login.js
@@ -1,8 +1,17 @@
-
 import axios from "axios";
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
+
+import { useSelector, useDispatch } from "react-redux";
+import { login } from "../reducers/login";
+
 const Login = () => {
+  const dispatch = useDispatch();
+  const state = useSelector((state) => {
+    return {
+      isLoggedIn: state.loginReducer.isLoggedIn,
+    };
+  });
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [userName, setUserName] = useState("");
@@ -10,47 +19,93 @@ const Login = () => {
   const [country, setCountry] = useState("");
   const [gender, setGender] = useState("");
   const [signuPMessage, setSignupMessage] = useState("");
+
+  /* ************************* */
+  const logInUser = async (e) => {
+    // e.preventDefault();
+
+    try {
+      const res = await axios.post("http://localhost:5000/login", {
+        userName,
+        password,
+      });
+      if (res.data.success) {
+        setSignupMessage ("");
+        localStorage.setItem("token", res.data.token);
+        dispatch(login(res.data.token));
+      } else throw Error;
+    } catch (error) {
+      if (error.response && error.response.data) {
+        return setSignupMessage(error.response.data.message);
+      }
+      setSignupMessage("Error happened while Login, please try again");
+    }
+  };
+  /* *************************** */
   return (
     <div>
+      <div>
+        <input
+          type="text"
+          placeholder="userName"
+          onChange={(e) => setUserName(e.target.value)}
+        />
+        <input
+          type="password"
+          placeholder="Password"
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <button
+          onClick={logInUser}
+        >
+          Login
+        </button>
+      </div>
       <div className="Sign_up">
         <input
           className="user_name"
-          type="text"  placeholder="userName"
+          type="text"
+          placeholder="userName"
           onChange={(e) => {
             setUserName(e.target.value);
           }}
         />
         <input
           className="email"
-          type="email"  placeholder="email"
+          type="email"
+          placeholder="email"
           onChange={(e) => {
             setEmail(e.target.value);
           }}
         />
         <input
           className="date_birth"
-          type="text" placeholder="date of birth"
+          type="text"
+          placeholder="date of birth"
           onChange={(e) => {
             setDate(e.target.value);
           }}
         />
         <input
           className="gender"
-          type="text"  placeholder="gender"
+          type="text"
+          placeholder="gender"
           onChange={(e) => {
             setGender(e.target.value);
           }}
         />
         <input
           className="country"
-          type="text"  placeholder="country"
+          type="text"
+          placeholder="country"
           onChange={(e) => {
             setCountry(e.target.value);
           }}
         />
         <input
           className="password"
-          type="password"   placeholder="password"
+          type="password"
+          placeholder="password"
           onChange={(e) => {
             setPassword(e.target.value);
           }}
