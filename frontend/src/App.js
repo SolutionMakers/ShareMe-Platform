15fcb137e7c80469ee830d42f12e72901afd2dc5
AbedalelahHyari <abedalelah.hyari@gmail.com>
2022-02-03 19:10:30 +0200

    Create the nav-bar and activate the logout action in loginReducer

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,16 +4,17 @@ import Login from "./components/Login";
 import SinglePostPage from "./components/SinglePostPage";
 import NewPost from "./components/NewPost";
 import Home from "./components/Home";
+import Navigation from "./components/Navigation";
 const App = () => {
   return (
     <div className="App">
+      <Navigation />
       <Routes>
         <Route path="/" element={<Login />} />
         <Route path="/NewPost" element={<NewPost />} />
         <Route path="/Home" element={<Home />} />
-          <Route path="/post/:id" element ={<SinglePostPage/>}/>
+        <Route path="/post/:id" element={<SinglePostPage />} />
       </Routes>
-
     </div>
   );
 };
