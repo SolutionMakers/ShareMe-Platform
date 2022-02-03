0a847a898f9f3b6c239a9067e73c9fd2c5f4b351
kholoud ibrahim <ahlam.aljawahreh@gmail.com>
2022-02-03 19:30:57 +0200

    get post likes

--- frontend/src/components/SinglePostPage.js
+++ frontend/src/components/SinglePostPage.js
@@ -10,94 +10,127 @@ const SinglePostPage = () => {
       token: state.loginReducer.token,
     };
   });
-  const [post, setPost] = useState({});
-  const [comments,setComments]= useState([]);
- const  [comment,setComment]=useState('');
+  const [post, setPost] = useState([]);
+  const [comments, setComments] = useState([]);
+  const [comment, setComment] = useState("");
+  const [likes, setLikes] = useState([]);
+  const [likeCount, setLikeCount] = useState(0);
+  /* ****************************************************** */
 
-
-const getCommentsByUserID= async ()=>{
-  try {
-    const res = await axios.get(`http://localhost:5000/all/${id}/comments`, {
-      headers: {
-        Authorization: `Bearer ${state.token}`,
-      },
-    });
-    if (res.data.success) {
-      setComments(res.data.results);
-    } 
-    
-  } catch (error) {
-    if (error.response && error.response.data) {
-     console.log(error.response.data.message);
+  const getCommentsByUserID = async () => {
+    try {
+      const res = await axios.get(`http://localhost:5000/all/${id}/comments`, {
+        headers: {
+          Authorization: `Bearer ${state.token}`,
+        },
+      });
+      if (res.data.success) {
+        setComments(res.data.results);
+      }
+    } catch (error) {
+      if (error.response && error.response.data) {
+        console.log(error.response.data.message);
+      }
     }
-    
-  }
-}
+  };
+  /* ****************************************************** */
 
+  const getLikesByUserID = async () => {
+    try {
+      const res = await axios.get(`http://localhost:5000/like/${id}`, {
+        headers: {
+          Authorization: `Bearer ${state.token}`,
+        },
+      });
+      if (res.data.success) {
+        setLikeCount(res.data.results.length);
+        setLikes(res.data.results);
+      }
+    } catch (error) {
+      if (error.response && error.response.data) {
+        console.log(error.response.data.message);
+      }
+    }
+  };
+  /* ****************************************************** */
   const getPostByID = async () => {
     try {
       const res = await axios.get(`http://localhost:5000/posts/${id}/post`);
       if (res.data.success) {
         setPost(res.data.results[0]);
-      } 
-      
+      }
     } catch (error) {
       if (error.response && error.response.data) {
-       console.log(error.response.data.message);
+        console.log(error.response.data.message);
       }
-      
     }
   };
-  const createNewComment = async ()=>{
-  try {
-    const res = await axios.post(`http://localhost:5000/posts/${id}/comments`,
-    { comment,},{
-      headers: {
-        Authorization: `Bearer ${state.token}`,
-      },
-    });
-    if (res.data.success) {
-      console.log('done')
-      setComment(res.data.results);
-    } 
-    
-  } catch (error) {
-    if (error.response && error.response.data) {
-     console.log(error.response.data);
+
+
+  /* ****************************************************** */
+  const createNewComment = async () => {
+    try {
+      const res = await axios.post(
+        `http://localhost:5000/posts/${id}/comments`,
+        { comment },
+        {
+          headers: {
+            Authorization: `Bearer ${state.token}`,
+          },
+        }
+      );
+      if (res.data.success) {
+        setComment(res.data.results);
+      }
+    } catch (error) {
+      if (error.response && error.response.data) {
+        console.log(error.response.data);
+      }
     }
-    
-  }
-}
+  };
+  /* ****************************************************** */
+
   useEffect(() => {
     getPostByID();
     getCommentsByUserID();
-  },[comment]);
-
+    getLikesByUserID();
+  }, [comment]);
   return (
     <div>
-       
+      {post ? (
         <>
           <p>{post.userName}</p>
           <img src={post.profileimage} />
           <p>{post.description}</p>
           <img src={post.media} />
-          {comments.length?comments.map((element,index)=>{
-            return (<div key ={index}>
-              <p>{element.userName}</p>
-              <img src={element.profileimage}/>
-              <p>{element.comment}</p>
-            </div>)
-          }):<></>}
+          <>
+            <p>{likeCount} LIKE</p> 
+          </>
+          {comments.length ? (
+            comments.map((element, index) => {
+              return (
+                <div key={index}>
+                  <p>{element.userName}</p>
+                  <img src={element.profileimage} />
+                  <p>{element.comment}</p>
+                </div>
+              );
+            })
+          ) : (
+            <></>
+          )}
+          <input
+            type="text"
+            placeholder="Whats on your Mind ?"
+            onChange={(e) => {
+              setComment(e.target.value);
+            }}
+          />
+          <button onClick={createNewComment}>Comment</button>
         </>
-        <input
-          type="text"
-          placeholder="Whats on your Mind ?"
-          onChange={(e) => {
-            setComment(e.target.value);
-          }}
-        />
-        <button onClick={createNewComment}>Comment</button>
-       
+      ) : (
+        <></>
+      )}
     </div>
   );
 };
