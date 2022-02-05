23a093f8f586b314f51616b8e2fbead9f4b2e8bd
BasharAbuHannoud <abuhannoud.bashar@gmail.com>
2022-02-05 21:23:23 +0200

    jxs home page and css

--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -1,6 +1,12 @@
 import React, { useState, useEffect } from "react";
 import { setPosts, updatePost, deletePost } from "../reducers/post/index";
 import { useSelector, useDispatch } from "react-redux";
+import { Routes, Route, Link, useNavigate } from "react-router-dom";
+import {
+  BsThreeDotsVertical,
+  BsFillHeartFill,
+  BsFillHandThumbsUpFill,
+} from "react-icons/bs";
 import axios from "axios";
 import noAvatar from "../images/noAvatar.png";
 /************************** */
@@ -8,7 +14,7 @@ const Home = () => {
   const [description, setDescription] = useState("");
   const [modal, setModal] = useState(false);
   const [id, setId] = useState("");
-
+const navigation=useNavigate();
   const toggleModal = (id) => {
     setModal(!modal);
 
@@ -76,71 +82,105 @@ const Home = () => {
     getAllPosts();
   }, []);
 
+  //------------------------------------------------------------
   return (
-    <div className="post">
-      <div>All Posts</div>
+    <div className="contain_all_home">
+      <div className="left_home"></div>
 
-      {state.posts.map((element, i) => {
-        return (
-          <div key={i}>
-            <div>
-              <div className="onePost">
-                <div>
-                  {" "}
-                  {modal && id == element.id && (
-                    <div className="modal">
-                      <div onClick={toggleModal} className="overlay"></div>
-                      <div className="modal-content">
-                        <h2>Hello Modal</h2>
-                        <button
-                          className="button_delete"
-                          onClick={() => handleDelete(element.id)}
-                        >
-                          delete
-                        </button>
-                        <input
-                          type="text"
-                          placeholder="updated description"
-                          onChange={(e) => {
-                            setDescription(e.target.value);
-                          }}
+      <div className="middle_home">
+        <div className="all_posts_home">
+          {state.posts.map((element, i) => {
+            return (
+              <div className="post">
+                <div className="postWrapper">
+                  <div className="postTop">
+                    <div className="postTopLeft">
+                      <Link to={`/profile/${element.id}`}>
+                        <img
+                          className="postProfileImg"
+                          width="100%"
+                          src={
+                            element.profileimage !== "undefined"
+                              ? element.profileimage
+                              : noAvatar
+                          }
                         />
-                        <button onClick={() => handleUpdate(element.id)}>
-                          Update
-                        </button>
-                        <button
-                          className="close-modal"
-                          onClick={() => toggleModal("")}
-                        >
-                          CLOSE
-                        </button>
+                      </Link>
+                      <span className="postUsername">{element.userName}</span>
+                      {/* <span className="postDate">{format(post.createdAt)}</span> */}
+                    </div>
+                    <div className="postTopRight">
+                      <BsThreeDotsVertical className="icon_popup"
+                        onClick={() => toggleModal(element.id)}
+                      />
+                      <div>
+                        {" "}
+                        {modal && id == element.id && (
+                          <div className="modal_post">
+                            <div
+                              onClick={toggleModal}
+                              className="overlay_post"
+                            ></div>
+                            <div className="modal-content_post">
+                              <h2>Edit Post</h2>
+                              <button
+                                className="button_delete"
+                                onClick={() => handleDelete(element.id)}
+                              >
+                                delete
+                              </button>
+                              <input
+                                type="text"
+                                placeholder="updated description"
+                                onChange={(e) => {
+                                  setDescription(e.target.value);
+                                }}
+                              />
+                              <button onClick={() => handleUpdate(element.id)}>
+                                Update
+                              </button>
+                              <button
+                                className="close-modal_post"
+                                onClick={() => toggleModal("")}
+                              >
+                                CLOSE
+                              </button>
+                            </div>
+                          </div>
+                        )}
                       </div>
                     </div>
-                  )}
+                  </div>
+
+                  <div className="postCenter">
+                    <p className="postText">{element.description}</p>
+                    <img
+                      className="postImg"
+                      src="https://amusd.net/wp-content/uploads/2021/03/HD-Wallpaper.jpg"
+                      alt=""
+                    />
+                  </div>
+
+                  <div className="postBottom">
+                    <div className="postBottomLeft">
+                      <BsFillHandThumbsUpFill className="likeIcon" />
+                      <BsFillHeartFill className="likeIcon_heart" />
+
+                      <span className="postLikeCounter">people like it</span>
+                    </div>
+                    <div className="postBottomRight">
+                      <span className="postCommentText" onClick={()=>{
+                        navigation(`/post/${element.id}`)
+                      }}> comments</span>
+                    </div>
+                  </div>
                 </div>
-
-                <div>{element.media}</div>
-                <div>{element.description}</div>
-                <img
-                  src={
-                    element.profileimage !== "undefined"
-                      ? element.profileimage
-                      : noAvatar
-                  }
-                />
-                <div>{element.userName}</div>
-
-                <button
-                  onClick={() => toggleModal(element.id)}
-                  className="btn-modal"
-                >
-                  My Modal
-                </button>
               </div>
-            </div>
-          </div>
-        );
-      })}
+            );
+          })}
+        </div>
+      </div>
+      <div className="rigth_home"></div>
     </div>
   );
 };
