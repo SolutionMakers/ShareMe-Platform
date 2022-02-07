7502920009d4bf94edef61d548163cea2f5cae32
AbedalelahHyari <abedalelah.hyari@gmail.com>
2022-02-07 08:11:54 +0200

    Connect the socket io to the backend server

--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,7 @@ const app = express();
 const db = require("./database/db");
 app.use(cors());
 app.use(express.json());
+const socket = require("socket.io");
 const PORT = 5000;
 /*************************************** */
 const loginRouter = require("./routes/login");
@@ -12,15 +13,26 @@ const usersRouter = require("./routes/users");
 const postsRouter = require("./routes/post");
 const commentsRouter = require("./routes/comments");
 const rolesRouter = require("./routes/roles");
-const likesRouter=require ("./routes/like")
+const likesRouter = require("./routes/like");
 /*************************************** */
 app.use("/login", loginRouter);
 app.use("/users", usersRouter);
 app.use("/posts", postsRouter);
 app.use("/", commentsRouter);
 app.use("/roles", rolesRouter);
-app.use("/like",likesRouter)
+app.use("/like", likesRouter);
 /*************************************** */
-app.listen(PORT, () => {
+const server = app.listen(PORT, () => {
   console.log(`SERVER WORKING ON PORT: ${PORT}`);
+});
+/******************************************** */
+const io = socket(server, {
+  cors: {
+    origin: "http://localhost:3000",
+    method: ["GET", "POST"],
+  },
+});
+
+io.on("connection", (socket) => {
+  console.log(`${socket.id} is connected`);
 });
