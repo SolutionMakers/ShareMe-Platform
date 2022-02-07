8ad93d407ec2238365879d937f0e3a88d17998a9
AbedalelahHyari <abedalelah.hyari@gmail.com>
2022-02-07 08:19:53 +0200

    Create new component called Chat That will show the converstions between users

---
+++ frontend/src/components/Chat/Chat.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "../Chat/Chat.css";
+
+const Chat = () => {
+  return (
+    <div>
+      <h1>This is Chat Section</h1>
+    </div>
+  );
+};
+
+export default Chat;
