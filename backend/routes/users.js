30cb02cce78962e2755a527eeb4d88357dbd2e97
AbedalelahHyari <abedalelah.hyari@gmail.com>
2022-02-07 20:05:01 +0200

    Add new end-point in the users router that will get all users

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -1,10 +1,16 @@
 const express = require("express");
 
-const { createNewUser ,updateProfilePhoto,getInfoUser} = require("../controllers/users");
+const {
+  createNewUser,
+  updateProfilePhoto,
+  getInfoUser,
+  getAllUsers,
+} = require("../controllers/users");
 
 const usersRouter = express.Router();
 
 usersRouter.post("/", createNewUser);
-usersRouter.put("/image/:id",updateProfilePhoto)
-usersRouter.get("/:id/info",getInfoUser)
+usersRouter.put("/image/:id", updateProfilePhoto);
+usersRouter.get("/:id/info", getInfoUser);
+usersRouter.get("/", getAllUsers);
 module.exports = usersRouter;
