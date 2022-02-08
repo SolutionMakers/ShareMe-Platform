08ad69625f45d797cff855d5f3d5e861fdde5059
AbedalelahHyari <abedalelah.hyari@gmail.com>
2022-02-08 12:31:56 +0200

    Create new function called joinRoomData that will excute  when the user click on the Chat button

--- frontend/src/components/Chat/Chat.js
+++ frontend/src/components/Chat/Chat.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { useParams, useNavigate } from "react-router-dom";
+import { useSelector } from "react-redux";
 import "../Chat/Chat.css";
 import { io } from "socket.io-client";
 import axios from "axios";
@@ -15,6 +16,14 @@ const Chat = () => {
   const [messageList, setMessageList] = useState([]);
   const [allUsers, setAllUsers] = useState([]);
   const { room } = useParams();
+  /********************************************* */
+  const state = useSelector((state) => {
+    return {
+      token: state.loginReducer.token,
+      posts: state.postsReducer.posts,
+      user_id: state.loginReducer.user_id,
+    };
+  });
   /************************************************************** */
   useEffect(() => {
     socket.on("RECEIVE_MESSAGE", (data) => {
@@ -52,17 +61,49 @@ const Chat = () => {
       console.log(err.response.data);
     }
   };
-
+  /*************************************************************** */
+  const joinRoomData = async (receiver_id) => {
+    try {
+      const res = await axios.post(
+        "http://localhost:5000/rooms",
+        {
+          id: receiver_id,
+        },
+        {
+          headers: {
+            Authorization: ` Bearer ${state.token}`,
+          },
+        }
+      );
+      // use the optional chaining to prevent the error when it access on the id and insertId
+      if (res.data.results[0]?.id) {
+        console.log(`done to open the room`);
+      }
+      if (res.data.results[0]?.insertId) {
+        console.log(`done to create the room`);
+      }
+    } catch (err) {
+      console.log(err);
+    }
+  };
+  /*********************************************************** */
   return (
     <>
       <div className="AllUsers">
         {allUsers.map((element, index) => {
           return (
-            <>
+            <div key={index}>
               <img src={element.profileimage} />
               <h1>{element.userName}</h1>
-              <button className="joinRoom">Chat</button>
-            </>
+              <button
+                onClick={() => {
+                  joinRoomData(element.id);
+                }}
+                className="joinRoom"
+              >
+                Chat
+              </button>
+            </div>
           );
         })}
       </div>
