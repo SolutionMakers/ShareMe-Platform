a272a7ff091d567a7af3b74a9dbe1093b3804121
AbedalelahHyari <abedalelah.hyari@gmail.com>
2022-02-11 00:00:41 +0200

    Add new route for the getAllFriendsByuserId function

--- backend/routes/friends.js
+++ backend/routes/friends.js
@@ -1,9 +1,10 @@
 const express = require("express");
 
-const { AddFriend } = require("../controllers/friends");
+const { AddFriend, getAllFriendsByUserId } = require("../controllers/friends");
 const { authentication } = require("../middleware/authentication");
 const friendsRouter = express.Router();
 
 friendsRouter.post("/", authentication, AddFriend);
+friendsRouter.get("/user", authentication, getAllFriendsByUserId);
 
 module.exports = friendsRouter;
