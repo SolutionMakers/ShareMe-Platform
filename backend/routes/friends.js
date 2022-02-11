38a7e1a2411f28c76d58c61e479b5ef5f0bbc4bc
AbedalelahHyari <abedalelah.hyari@gmail.com>
2022-02-11 12:31:41 +0200

    Add route to the removeFriend function in the friends router

--- backend/routes/friends.js
+++ backend/routes/friends.js
@@ -4,6 +4,7 @@ const {
   AddFriend,
   getAllFriendsByUserId,
   getAllFriends,
+  removeFriend,
 } = require("../controllers/friends");
 const { authentication } = require("../middleware/authentication");
 const friendsRouter = express.Router();
@@ -11,5 +12,6 @@ const friendsRouter = express.Router();
 friendsRouter.post("/", authentication, AddFriend);
 friendsRouter.get("/user", authentication, getAllFriendsByUserId);
 friendsRouter.get("/all", getAllFriends);
+friendsRouter.put("/remove/:id", authentication, removeFriend);
 
 module.exports = friendsRouter;
