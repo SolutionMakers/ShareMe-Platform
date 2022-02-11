5aeeba838b7dac1796df3ec45926628a4cff3ea5
AbedalelahHyari <abedalelah.hyari@gmail.com>
2022-02-11 15:54:03 +0200

    Add condtion in the Chat component that check if the user have image or not

--- frontend/src/components/Chat/Chat.js
+++ frontend/src/components/Chat/Chat.js
@@ -4,7 +4,7 @@ import { useSelector } from "react-redux";
 import "../Chat/Chat.css";
 import { io } from "socket.io-client";
 import axios from "axios";
-
+import noAvatar from "../../images/noAvatar.png";
 const ENDPOINT = "http://localhost:5000";
 const socket = io.connect(ENDPOINT);
 
@@ -130,45 +130,47 @@ const Chat = () => {
   return (
     <>
       <div className="All_chat">
-
         <div className="left_side_chat">
-        {allUsers.map((element, index) => {
-          return (
-
-         
-            <div key={index} >
-
-<div className="name_img">
-              <img src={element.profileimage} className="img_user_chat" />
-              <div>{element.userName}</div>
-              <button
-                onClick={() => {
-                  joinRoomData(element.id);
-                }}
-                className="joinRoom"
-              >
-                Chat
-              </button>
+          {allUsers.map((element, index) => {
+            return (
+              <div key={index}>
+                <div className="name_img">
+                  <img
+                    src={
+                      element.profileimage !== "undefined"
+                        ? element.profileimage
+                        : noAvatar
+                    }
+                    className="img_user_chat"
+                  />
+                  <div>{element.userName}</div>
+                  <button
+                    onClick={() => {
+                      joinRoomData(element.id);
+                    }}
+                    className="joinRoom"
+                  >
+                    Chat
+                  </button>
+                </div>
               </div>
-            
-              
-            </div>
+            );
+          })}
+        </div>
 
-
-          );
-        })}
-  </div>
-
-      <div className="mid_side_chat">
-        {loggedIn ? (
-          <div className="chat_page">
-          
+        <div className="mid_side_chat">
+          {loggedIn ? (
+            <div className="chat_page">
               {messageList.length ? (
                 messageList.map((element, index) => {
                   return (
                     <div key={index} className="chat_rod">
                       <div className="words_chat">
-                        <img className="img_user_chat" src={element.profileimage}/> <div className="message">{element.message}</div>
+                        <img
+                          className="img_user_chat"
+                          src={element.profileimage}
+                        />{" "}
+                        <div className="message">{element.message}</div>
                       </div>
                     </div>
                   );
@@ -176,36 +178,30 @@ const Chat = () => {
               ) : (
                 <></>
               )}
-      
 
+              <input
+                className="input_send_chat"
+                type={"text"}
+                placeholder="Message ..."
+                onChange={(e) => {
+                  setMessage(e.target.value);
+                }}
+              />
+              <button
+                onClick={createMessage}
+                style={{ backgroundColor: "#4ba9d4" }}
+              >
+                send
+              </button>
+            </div>
+          ) : (
+            <div>
+              <button onClick={joinRoom}>Enter Room</button>
+            </div>
+          )}
+        </div>
 
-            <input className="input_send_chat"
-             
-              type={"text"}
-              placeholder="Message ..."
-              onChange={(e) => {
-                setMessage(e.target.value);
-              }}
-            />
-            <button
-              onClick={createMessage}
-              style={{ backgroundColor: "#4ba9d4" }}
-            >
-              send
-            </button>
-
-
-          </div>
-        ) : (
-          <div>
-           
-           
-            <button onClick={joinRoom}>Enter Room</button>
-          </div>
-        )}
-      </div>
-
-<div className="right_side_chat">Details</div>
+        <div className="right_side_chat">Details</div>
       </div>
     </>
   );
