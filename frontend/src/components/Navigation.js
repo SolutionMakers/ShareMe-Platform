fde89894dfddab7268f3050c7d3546c0228884e5
BasharAbuHannoud <abuhannoud.bashar@gmail.com>
2023-01-14 16:23:56 +0300

    fixed drop down navigtion

--- frontend/src/components/Navigation.js
+++ frontend/src/components/Navigation.js
@@ -1,13 +1,14 @@
-import { logout } from "../reducers/login";
-import { useDispatch, useSelector } from "react-redux";
-import { AiFillHome } from "react-icons/ai";
-import { Routes, Route, Link, useNavigate } from "react-router-dom";
-import React, { useState, useEffect } from "react";
+import {logout} from "../reducers/login";
+import {useDispatch, useSelector} from "react-redux";
+import {AiFillHome} from "react-icons/ai";
+import {Link, useNavigate} from "react-router-dom";
+import React, {useState, useEffect, useRef} from "react";
 import axios from "axios";
 import noAvatar from "../images/noAvatar.png";
-import { FaFacebookMessenger } from "react-icons/fa";
-import { BsSearch } from "react-icons/bs";
-import { MdOutlineLogout } from "react-icons/md";
+import {FaFacebookMessenger} from "react-icons/fa";
+import {BsSearch} from "react-icons/bs";
+import {MdOutlineLogout} from "react-icons/md";
+
 const Navigation = () => {
   const dispatch = useDispatch();
   const navigate = useNavigate();
@@ -37,7 +38,7 @@ const Navigation = () => {
     }
   };
   const filteredSearch = (str) => {
-    const result = allUsers.filter((element, i) => {
+    const result = allUsers.filter((element) => {
       return element.userName.includes(str);
     });
     setSearchResult(result);
@@ -47,120 +48,127 @@ const Navigation = () => {
   const toggleModal = () => {
     setModal(!modal);
   };
+
+  const ref = useRef();
+
+  useEffect(() => {
+
+    const handleClick = (event) => {
+      if (ref.current && !ref.current.contains(event.target)) {
+        toggleModal()
+      }
+    };
+
+    document.addEventListener('click', handleClick);
+
+    return () => {
+      document.removeEventListener('click', handleClick);
+    };
+  }, []);
+
+
   /***************************************************************************************** */
   useEffect(() => {
-    getAllUsers();
+    getAllUsers().then(res => console.log('All users done'));
   }, []);
   /***************************************************************************************** */
 
   return (
-    <>
       <div className="nav">
         <div className="logo_search">
-          <img className="logo" src="https://res.cloudinary.com/dvg9eijgb/image/upload/v1645002900/su1xnqw4k9jxcaxmyuwu.png" onClick={()=>{
-             navigate(`/home`);
-          }}/>
-            
-      
+          <img className="logo" alt="logo-friends"
+               src="https://res.cloudinary.com/dvg9eijgb/image/upload/v1645002900/su1xnqw4k9jxcaxmyuwu.png"
+               onClick={() => {
+                 navigate(`/home`);
+               }}/>
+
 
           <div className="input_and_icon">
             <div className="search">
               <input
-                className="input_search"
-                type="text"
-                value={typing}
-                placeholder="Find a friend"
-                onChange={(e) => {
-                  setTyping(e.target.value);
-                  filteredSearch(typing);
-                  if (e.target.value !== "") {
-                    setModal(true);
-                  } else {
-                    setModal(false);
-                  }
-                }}
+                  className="input_search"
+                  type="text"
+                  value={typing}
+                  placeholder="Find a friend"
+                  onChange={(e) => {
+                    setTyping(e.target.value);
+                    filteredSearch(typing);
+                    if (e.target.value !== "") {
+                      setModal(true);
+                    } else {
+                      setModal(false);
+                    }
+                  }}
               />
+
+
+              {modal && <div className="rod" ref={ref}>
+                {searchResult.length ? (searchResult.map((user) => {
+                  return (<>
+                    <div
+                        className="user_info_rod"
+                        onClick={() => {
+                          navigate(`/profile/${user.id}`);
+                          toggleModal();
+                          setTyping("");
+                        }}
+                    >
+                      <img
+                          alt="profile-img"
+                          src={user.profileimage !== "undefined" ? user.profileimage : noAvatar}
+                          className="img_search"
+                      />
+                      <span>{user.userName}</span>
+                    </div>
+                  </>);
+                })) : (<>No results !</>)}
+              </div>}
+
+
             </div>
 
-            <BsSearch className="icon_search" />
+            <BsSearch className="icon_search"/>
           </div>
 
-          {modal && (
-            <div className="modal_search">
-              <div onClick={toggleModal} className="overlay_search"></div>
-              <div className="modal-content_search">
-                <div className="rod">
-                  {searchResult.length ? (
-                    searchResult.map((user) => {
-                      return (
-                        <>
-                          <div
-                            className="user_info_rod"
-                            onClick={() => {
-                              navigate(`/profile/${user.id}`);
-                              toggleModal();
-                              setTyping("");
-                            }}
-                          >
-                            <img
-                              src={
-                                user.profileimage !== "undefined"
-                                  ? user.profileimage
-                                  : noAvatar
-                              }
-                              className="img_search"
-                            />
-                            <span>{user.userName}</span>
-                          </div>
-                        </>
-                      );
-                    })
-                  ) : (
-                    <></>
-                  )}
-                </div>
-              </div>
-            </div>
-          )}
+
         </div>
 
         <FaFacebookMessenger
-          className="chat"
-          onClick={() => {
-            navigate(`/chat`);
-          }}
+            className="chat"
+            onClick={() => {
+              navigate(`/chat`);
+            }}
         />
         <div className="home_nav">
           <Link to="/home">
-            <AiFillHome className="home_icon" />
+            <AiFillHome className="home_icon"/>
           </Link>
         </div>
 
         <div
-          className="user_nav"
-          onClick={() => {
-            navigate(`/profile/${state.user_id}`);
-          }}
+            className="user_nav"
+            onClick={() => {
+              navigate(`/profile/${state.user_id}`);
+            }}
         >
           <img
-            className="img_user_nav"
-            src={imgUser !== "undefined" ? imgUser : noAvatar}
+              className="img_user_nav"
+              src={imgUser !== "undefined" ? imgUser : noAvatar}
           />
           <div className="userName_font_nav">{state.userName}</div>
         </div>
         <div className="style_logOut">
           <MdOutlineLogout
-            className="LogOut"
-            onClick={() => {
-              dispatch(logout());
-              localStorage.clear();
-              navigate("/");
-            }}
+              className="LogOut"
+              onClick={() => {
+                dispatch(logout());
+                localStorage.clear();
+                navigate("/");
+              }}
           />
-        
+
         </div>
       </div>
-    </>
   );
 };
 
